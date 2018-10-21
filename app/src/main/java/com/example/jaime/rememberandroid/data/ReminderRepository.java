50bb0e71626cb0d07f569d500dd0f5a334be1d0d
JaimePerezS <jaimeps93@gmail.com>
2018-10-22 00:30:43 +0200

    Implemented Repository pattern.

---
+++ app/src/main/java/com/example/jaime/rememberandroid/data/ReminderRepository.java
@@ -0,0 +1,41 @@
+package com.example.jaime.rememberandroid.data;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class ReminderRepository {
+    private ReminderDao mReminderDao;
+    private LiveData<List<Reminder>> mAllReminders;
+
+    public ReminderRepository(Application application) {
+        ReminderDatabase db = ReminderDatabase.getDatabase(application);
+        this.mReminderDao = db.reminderDao();
+        this.mAllReminders = mReminderDao.getReminders();
+    }
+
+    public LiveData<List<Reminder>> getReminders() {
+        return mAllReminders;
+    }
+
+    public void insertReminder(Reminder reminder) {
+        new insertAsyncTask(mReminderDao).execute(reminder);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Reminder, Void, Void> {
+        private ReminderDao mAsyncTaskDao;
+
+        insertAsyncTask(ReminderDao dao) {
+            this.mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Reminder... params){
+            mAsyncTaskDao.saveReminder(params[0]);
+            return null;
+        }
+    }
+
+}
