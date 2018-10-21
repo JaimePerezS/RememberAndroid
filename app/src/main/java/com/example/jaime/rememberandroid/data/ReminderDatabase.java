455b9e2e1708ec2b609902b3d6f71a8106b4eba9
JaimePerezS <jaimeps93@gmail.com>
2018-10-22 00:17:45 +0200

    Create database class using Room.

---
+++ app/src/main/java/com/example/jaime/rememberandroid/data/ReminderDatabase.java
@@ -0,0 +1,24 @@
+package com.example.jaime.rememberandroid.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Reminder.class}, version = 1)
+public abstract class ReminderDatabase extends RoomDatabase {
+    public abstract ReminderDao reminderDao();
+
+    private static volatile ReminderDatabase INSTANCE;
+
+    static ReminderDatabase getDatabase(final Context context) {
+        if(INSTANCE == null) {
+            synchronized (ReminderDatabase.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), ReminderDatabase.class, "reminder_database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
