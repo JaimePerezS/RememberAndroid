9129baeb487bbba3570bae2421ed2ebc01fbda5b
JaimePerezS <jaimeps93@gmail.com>
2018-10-22 00:18:43 +0200

    Added LiveData to ReminderDao.

--- app/src/main/java/com/example/jaime/rememberandroid/data/ReminderDao.java
+++ app/src/main/java/com/example/jaime/rememberandroid/data/ReminderDao.java
@@ -1,5 +1,6 @@
 package com.example.jaime.rememberandroid.data;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
@@ -13,7 +14,7 @@ import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 @Dao
 public interface ReminderDao {
     @Query("SELECT * FROM reminders")
-    List<Reminder> getReminders();
+    LiveData<List<Reminder>> getReminders();
 
     @Insert(onConflict = REPLACE)
     void saveReminder(Reminder reminder);
