c9171244b86e21a38ba5eadf74b36f9cc344a3e8
JaimePerezS <jaimeps93@gmail.com>
2018-10-21 20:36:31 +0200

    Reminder Entity added.

---
+++ app/src/main/java/com/example/jaime/rememberandroid/data/Reminder.java
@@ -0,0 +1,45 @@
+package com.example.jaime.rememberandroid.data;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "reminders")
+public class Reminder {
+    @PrimaryKey
+    private int id;
+    private String name;
+    private String date;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
