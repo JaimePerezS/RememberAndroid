8522136aedbca1acdec69c080608db358e60d45b
JaimePerezS <jaimeps93@gmail.com>
2018-10-22 01:13:18 +0200

    Implemented Reminders RecyclerView.

--- app/src/main/java/com/example/jaime/rememberandroid/ReminderListFragment.java
+++ app/src/main/java/com/example/jaime/rememberandroid/ReminderListFragment.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.jaime.rememberandroid.adapters.ReminderAdapter;
 
 
 /**
@@ -64,7 +68,14 @@ public class ReminderListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminder_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_reminder_list, container, false);
+
+        RecyclerView recyclerView = v.findViewById(R.id.rcViewReminderList);
+        final ReminderAdapter adapter = new ReminderAdapter(this.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -80,8 +91,7 @@ public class ReminderListFragment extends Fragment {
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+
         }
     }
 
---
+++ app/src/main/java/com/example/jaime/rememberandroid/adapters/ReminderAdapter.java
@@ -0,0 +1,68 @@
+package com.example.jaime.rememberandroid.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.jaime.rememberandroid.R;
+import com.example.jaime.rememberandroid.data.Reminder;
+
+import java.util.List;
+
+public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ReminderViewHolder> {
+
+    class ReminderViewHolder extends RecyclerView.ViewHolder {
+        private final TextView txtViewName;
+        private final TextView txtViewDate;
+
+        private ReminderViewHolder(View v) {
+            super(v);
+            txtViewName = v.findViewById(R.id.txtViewName);
+            txtViewDate = v.findViewById(R.id.txtViewDate);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<Reminder> mRemindersList;
+
+    public ReminderAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public ReminderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = mInflater.inflate(R.layout.item_reminder_layout, parent, false);
+        return new ReminderViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReminderViewHolder holder, int position) {
+        if(mRemindersList != null) {
+            Reminder currentReminder = mRemindersList.get(position);
+            holder.txtViewName.setText(currentReminder.getName());
+            holder.txtViewDate.setText(currentReminder.getDate());
+        } else {
+            holder.txtViewName.setText("Empty");
+            holder.txtViewDate.setText("");
+        }
+    }
+
+    void setReminders(List<Reminder> reminders) {
+        mRemindersList = reminders;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mRemindersList != null) {
+            return mRemindersList.size();
+        } else {
+            return 0;
+        }
+    }
+}
