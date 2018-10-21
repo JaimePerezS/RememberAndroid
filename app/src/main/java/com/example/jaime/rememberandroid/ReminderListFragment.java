1f2ea9aafbc166a701c1cb2e360b7d9c20a7d73c
JaimePerezS <jaimeps93@gmail.com>
2018-10-22 01:18:01 +0200

    Clear comments.

--- app/src/main/java/com/example/jaime/rememberandroid/ReminderListFragment.java
+++ app/src/main/java/com/example/jaime/rememberandroid/ReminderListFragment.java
@@ -13,21 +13,11 @@ import android.view.ViewGroup;
 import com.example.jaime.rememberandroid.adapters.ReminderAdapter;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ReminderListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ReminderListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ReminderListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
+    
     private String mParam1;
     private String mParam2;
 
@@ -37,15 +27,6 @@ public class ReminderListFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReminderListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ReminderListFragment newInstance(String param1, String param2) {
         ReminderListFragment fragment = new ReminderListFragment();
         Bundle args = new Bundle();
@@ -67,7 +48,6 @@ public class ReminderListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_reminder_list, container, false);
 
         RecyclerView recyclerView = v.findViewById(R.id.rcViewReminderList);
@@ -78,7 +58,6 @@ public class ReminderListFragment extends Fragment {
         return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -101,16 +80,6 @@ public class ReminderListFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
